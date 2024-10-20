fc227f0ebaefc4ad1904ab165739674bdbfa0ae0
Ethan Lanting <ethanlanting@gmail.com>
2024-10-20 16:23:54 -0500

    add map feature

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,19 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
+import MapView, { Callout, Details, Marker, Region } from "react-native-maps";
+import { Airplay, Camera } from "@tamagui/lucide-icons";
+import { Button } from "tamagui";
 
 export default function Index() {
+  const [draggableMarkerCoord, setDraggableMarkerCoord] = useState({
+    latitude: 38.8977,
+    longitude: -77.0365,
+  });
+
+  const onRegionChange = (region: Region, details: Details) => {
+    console.log(region, details);
+  };
+
   return (
     <View
       style={{
@@ -9,7 +22,69 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <MapView style={styles.map} onRegionChange={onRegionChange}>
+        <Marker
+          draggable
+          coordinate={draggableMarkerCoord}
+          onDragEnd={(e) => setDraggableMarkerCoord(e.nativeEvent.coordinate)}
+          title="Drag me!"
+          description="This is a draggable marker."
+        >
+          <Callout
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: 200,
+              height: 80,
+              backgroundColor: "white",
+              borderRadius: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Custom Callout
+            </Text>
+            <Button
+              title="Press me"
+              onPress={() => console.log("Button pressed")}
+            />
+          </Callout>
+        </Marker>
+
+        <View style={styles.mapOverlay}>
+          <Button backgroundColor="$blue4" icon={Camera}>
+            test
+          </Button>
+          <Button alignSelf="center" icon={Airplay} size="$6">
+            Large
+          </Button>
+        </View>
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+    padding: 12,
+  },
+  mapOverlay: {
+    position: "absolute",
+    bottom: 40,
+    borderWidth: 2,
+    borderRadius: 20,
+    overflow: "hidden",
+    padding: 10,
+    width: "100%",
+    left: 12,
+    textAlign: "center",
+    zIndex: 100,
+  },
+});
